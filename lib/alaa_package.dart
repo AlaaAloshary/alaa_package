eaf2e0515d17030b21dc7c547ce27e2a056d8c0e
AlaaAloshary <alaanoaman33@gmail.com>
2024-11-28 10:45:40 +0300

    edit Awesome Snackbar Content

--- lib/alaa_package.dart
+++ lib/alaa_package.dart
@@ -6,3 +6,4 @@ export 'package:json_theme/json_theme.dart';
 export 'package:json_class/json_class.dart';
 export 'package:json_schema/json_schema.dart';
 export 'package:json_theme_annotation/json_theme_annotation.dart';
+export 'package:awesome_snackbar_content/awesome_snackbar_content.dart';
